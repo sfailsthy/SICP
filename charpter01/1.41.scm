fc990dc0274c9d0453b3d4adf1c5c31d6975931e
sfailsthy <z1823339284@gmail.com>
2017-12-07 18:18:47 +0800

    almost finish charpter01

---
+++ charpter01/1.41.scm
@@ -0,0 +1,9 @@
+#lang scheme
+(define (double f)
+  (lambda (x)
+    (f (f x))))
+
+(define (inc x)
+  (+ 1 x))
+
+(((double (double double)) inc) 5)
---
+++ charpter01/1.42.scm
@@ -0,0 +1,12 @@
+#lang scheme
+(define (compose f g)
+  (lambda (x)
+    (f (g x))))
+
+(define (inc x)
+  (+ 1 x))
+
+(define (square x)
+  (* x x))
+
+((compose square inc) 6)
