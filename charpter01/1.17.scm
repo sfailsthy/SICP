4ab060ce9d449ae29db1607995f458e40078e029
sfailsthy <z1823339284@gmail.com>
2017-12-07 16:21:04 +0800

    charpter01

---
+++ charpter01/1.17.scm
@@ -0,0 +1,13 @@
+#lang scheme
+(define (double n)
+  (+ n n))
+(define (halve n)
+  (/ n 2))
+
+(define (multi a b)
+  (cond ((= b 0)
+         0)
+        ((even? b)
+         (double (multi a (halve b))))
+        (else
+         (+ a (multi a (- b 1))))))
---
+++ charpter01/1.27.scm
@@ -0,0 +1,27 @@
+#lang scheme
+(define (carmichael-test n)
+  (test-iter 1 n))
+
+(define (test-iter a n)
+  (cond ((= a n)
+         true)
+        ((congruent? a n)
+         (test-iter (+ a 1) n))
+        (else
+         false)))
+
+(define (congruent? a n)
+  (= (expmod a n n) a))
+
+(define (expmod base exp m)
+  (cond ((= exp 0)
+         1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(define (square x)
+  (* x x))
