7a8cbad3e9bef7ffe61496eaadb3794ad66b41f4
sfailsthy <z1823339284@gmail.com>
2017-12-09 17:51:49 +0800

    delete charpter01/

--- charpter01/1.41.scm
+++
@@ -1,8 +0,0 @@
-(define (double f)
-  (lambda (x)
-    (f (f x))))
-
-(define (inc x)
-  (+ 1 x))
-
-(((double (double double)) inc) 5)
--- charpter01/average-damp.scm
+++
@@ -1,5 +0,0 @@
-(define (average-damp f)
-  (lambda (x) (average x (f x))))
-
-(define (average x y)
-  (/ (+ x y) 2))
