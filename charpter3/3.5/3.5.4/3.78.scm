b6c384bc4ddef8062b93c06a78e2796b9da7b4c3
sfailsthy <z1823339284@gmail.com>
2017-12-20 20:42:43 +0800

    update 3.78.scm&3.79.scm

--- charpter3/3.5/3.5.4/3.78.scm
+++ charpter3/3.5/3.5.4/3.78.scm
@@ -1,6 +1,6 @@
 (load "solve-exp.scm")
 
-(define (solv-2nd a b dt y0 dy0)
+(define (solve-2nd a b dt y0 dy0)
   (define y (integral (delay dy) y0 dt))
   (define dy (integral (delay ddy) dy0 dt))
   (define ddy (add-streams (scale-stream dy a)
--- charpter3/3.5/3.5.4/3.79.scm
+++ charpter3/3.5/3.5.4/3.79.scm
@@ -1,6 +1,6 @@
 (load "solve-exp.scm")
 
-(define (general-solv-2nd a b dt y0 dy0)
+(define (general-solve-2nd f a b dt y0 dy0)
   (define y (integral (delay dy) y0 dt))
   (define dy (integral (delay ddy) dy0 dt))
   (define ddy (stream-map f dy y))
